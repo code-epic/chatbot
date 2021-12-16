e29c5deb8bbd5f504c6c3e0c75e4306d45bce3a8
Carlos Peña <gesaodin@gmail.com>
2021-12-15 21:19:34 -0400

    Actualizacion de la Interfaz

--- src/app/services/chatbot.service.ts
+++ src/app/services/chatbot.service.ts
@@ -49,89 +49,95 @@ export interface IToken{
 export class ChatbotService {
 
   style = `.media {
-              padding: 16px 12px;
-              -webkit-transition: background-color .2s linear;
-              transition: background-color .2s linear;
-            }
-            .media-chat {
-              padding-right: 64px;
-              margin-bottom: 0;
-            }
+    padding: 16px 12px;
+    -webkit-transition: background-color .2s linear;
+    transition: background-color .2s linear;
+  }
+  .media-chat {
+    padding-right: 64px;
+    margin-bottom: 0;
+  }
 
-            .media .avatar {
-              flex-shrink: 0;
-            }
-            .avatar {
-              position: relative;
-              display: inline-block;
-              width: 56px;
-              height: 56px;
-              line-height: 56px;
-              text-align: center;
-              border-radius: 100%;
-              background-color: #f5f6f7;
-              color: #8b95a5;
-              text-transform: uppercase;
-            }
-            .media-chat .media-body {
-              -webkit-box-flex: initial;
-              flex: initial;
-              display: table;
-            }
-            .media-body {
-              min-width: 0;
-            }
-            .media-chat .media-body p {
-              position: relative;
-              padding: 6px 8px;
-              margin: 4px 0;
-              background-color: #f5f6f7;
-              border-radius: 3px;
-              font-weight: 100;
-              color: #000000;
-            }
-            .media>* {
-              margin: 0 8px;
-            }
-            .media-chat .media-body p.meta {
-              background-color: transparent !important;
-              color: #3082d3;
-              padding: 0;
-              opacity: .8;
-            }
-            
-            .media-chat.media-chat-reverse {
-              padding-right: 12px;
-              padding-left: 64px;
-              -webkit-box-orient: horizontal;
-              -webkit-box-direction: reverse;
-              flex-direction: row-reverse;
-          }
-          
-          .media-chat {
-              padding-right: 64px;
-              margin-bottom: 0;
-          }
-          
-          
-          
-          .media-chat.media-chat-reverse .media-body p {
-              float: right;
-              clear: right;
-              background-color: #48b0f7;
-              color: #fff;
-          }
-          
-          .media-chat.media-chat-reverse .media-body p.meta {
-              background-color: transparent !important;
-              color: #3082d3;
-              padding: 0;
-              opacity: .8;
-          }
-            
-            
-            `
+  .media .avatar {
+    flex-shrink: 0;
+  }
+
+  .logo {
+    width: 20%;
+    height: 20%;
+  }
+
+  .avatar {
+    position: relative;
+    display: inline-block;
+    width: 56px;
+    height: 56px;
+    line-height: 56px;
+    text-align: center;
+    border-radius: 100%;
+    background-color: #f5f6f7;
+    color: #8b95a5;
+    text-transform: uppercase;
+  }
+  .media-chat .media-body {
+    -webkit-box-flex: initial;
+    flex: initial;
+    display: table;
+  }
+  .media-body {
+    min-width: 0;
+  }
+  .media-chat .media-body p {
+    position: relative;
+    padding: 6px 8px;
+    margin: 4px 0;
+    background-color: #C3E7E7;
+    border-radius: 25px;
+    font-weight: 100;
+    color: #000000;
+  }
+  .media>* {
+    margin: 0 8px;
+  }
+  .media-chat .media-body p.meta {
+    background-color: transparent !important;
+    color: #3082d3;
+    padding: 0;
+    opacity: .8;
+  }
   
+  .media-chat.media-chat-reverse {
+    padding-right: 12px;
+    padding-left: 64px;
+    -webkit-box-orient: horizontal;
+    -webkit-box-direction: reverse;
+    flex-direction: row-reverse;
+}
+
+.media-chat {
+    padding-right: 64px;
+    margin-bottom: 0;
+}
+
+
+
+.media-chat.media-chat-reverse .media-body p {
+    float: right;
+    clear: right;
+    background-color: #48b0f7;
+    color: #fff;
+}
+
+.media-chat.media-chat-reverse .media-body p.meta {
+    background-color: transparent !important;
+    color: #3082d3;
+    padding: 0;
+    opacity: .8;
+}
+  
+  
+  `
+
   URL =  '/v1/api/';
   urlGet : string
    
